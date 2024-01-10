de8054221c78ed6aa7b74138bf5b42e605e43370
Zinnurov Artyom <zinnurov.artyom.zinnurov@yandex.ru>
2024-01-10 23:15:17 +0300

    adding pre-commit

--- mlops_project/model.py
+++ mlops_project/model.py
@@ -3,88 +3,89 @@ import torch
 
 class ReLU(torch.nn.Module):
     def __init__(self):
-        '''
+        """
         Слой ReLU поэлементно применяет Rectified Linear Unit к своему входу
-        '''
+        """
         super().__init__()
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        '''
+        """
         Применяет ReLU ко входному тензору
-        '''
+        """
         return torch.nn.functional.relu(x)
-    
+
 
 class Linear(torch.nn.Module):
     def __init__(self, in_features, out_features):
-        '''
+        """
         Полносвязный слой — это слой выполняющий аффинное преобразование f(x) = x W + b
-        '''
+        """
         super().__init__()
-        
+
         # Создание необходимых обучаемых параметров
-        self.weight = torch.nn.parameter.Parameter(torch.empty(in_features, out_features))
+        self.weight = torch.nn.parameter.Parameter(
+            torch.empty(in_features, out_features)
+        )
         self.bias = torch.nn.parameter.Parameter(torch.empty(out_features))
-        
+
         # Выполнение инициализации весов
         self.reset_parameters()
 
-        
     def reset_parameters(self):
-        '''
-        Инициализация весов полносвязного слоя из нормального распределения с 
+        """
+        Инициализация весов полносвязного слоя из нормального распределения с
             нулевым средним и стандартным отклонением 0.01
         Вектор-смещение инициализируется нулями
-        '''
+        """
         torch.nn.init.normal_(self.weight, mean=0, std=0.01)
         torch.nn.init.zeros_(self.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        '''
+        """
         Выполнение аффинного преобразования f(x) = x W + b
-        
+
         :param torch.Tensor x: входная матрица размера [batch_size, in_features]
         :returns: матрица размера [batch_size, out_features]
-        '''
+        """
         return x @ self.weight + self.bias
-    
-    def __repr__(self):
-        '''
-        Опциональный метод для красивого вывода
-        '''
-        return 'Linear({0:d}, {1:d})'.format(*self.weight.shape)
 
-    
+    def __repr__(self):
+        """
+        Опциональный метод для красивого вывода
+        """
+        return "Linear({0:d}, {1:d})".format(*self.weight.shape)
+
+
 class CrossEntropyLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        
+
     def forward(self, x: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
         """
         Применение логсофтмакса к каждой строке, а затем выборка элементов в соответствии с метками истинного класса
         :param torch.Tensor x: Матрица логитов размера [batch_size, n_classes]
         :param torch.Tensor labels: Список меток истинного класса. Размер [batch_size]
-        :returns: Кросс-энтропийная функция потерь 
+        :returns: Кросс-энтропийная функция потерь
         """
         batch_size = labels.shape[0]
-        max_x = torch.max(x, 1)[0].view(-1, 1)              # (batch_size)x1
-        exp_norm_x = torch.exp(x - max_x)                   # (batch_size)x(n_classes)
-        sum_exp_norm = torch.sum(exp_norm_x, 1).view(-1, 1) # (batch_size)x1
-        softmax = exp_norm_x / sum_exp_norm                 # (batch_size)x(n_classes)
-        return -torch.mean(torch.log(softmax[range(batch_size),labels])) # const
+        max_x = torch.max(x, 1)[0].view(-1, 1)  # (batch_size)x1
+        exp_norm_x = torch.exp(x - max_x)  # (batch_size)x(n_classes)
+        sum_exp_norm = torch.sum(exp_norm_x, 1).view(-1, 1)  # (batch_size)x1
+        softmax = exp_norm_x / sum_exp_norm  # (batch_size)x(n_classes)
+        return -torch.mean(torch.log(softmax[range(batch_size), labels]))  # const
 
 
 class DenseNetwork(torch.nn.Module):
     def __init__(self, in_features, hidden_size, n_classes, n_layers, activation=ReLU):
-        '''
+        """
         :param int in_features: Число входных признаков
         :param int hidden_size: Размер скрытых слоёв
-        :param int n_classes: Число выходов сети 
+        :param int n_classes: Число выходов сети
         :param int n_layers: Число слоёв в сети
         :param torch.nn.Module activation: Класс функции активации
-        '''
+        """
         super().__init__()
-        
+
         in_feat = None
         out_feat = None
         self.layers = torch.nn.Sequential()
@@ -106,9 +107,9 @@ class DenseNetwork(torch.nn.Module):
                 self.layers.append(activation())
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        '''
+        """
         Прямой проход по сети
         :param torch.Tensor x: Входной тензор размера [batch_size, in_features]
         :returns: Матрица логитов размера [batch_size, n_classes]
-        '''
+        """
         return self.layers(x)
--- mlops_project/train.py
+++ mlops_project/train.py
@@ -3,7 +3,7 @@ import tqdm.notebook as tqdm
 
 
 def training_loop(n_epochs, network, loss_fn, optimizer, ds_train, ds_test, device):
-    '''
+    """
     :param int n_epochs: Число итераций оптимизации
     :param torch.nn.Module network: Нейронная сеть
     :param Callable loss_fn: Функция потерь
@@ -12,7 +12,7 @@ def training_loop(n_epochs, network, loss_fn, optimizer, ds_train, ds_test, devi
     :param Tuple[torch.Tensor, torch.Tensor] ds_test: Признаки и метки истинного класса тестовой выборки
     :param torch.Device device: Устройство на котором будут происходить вычисления
     :returns: Списки значений функции потерь и точности на обучающей и тестовой выборках после каждой итерации
-    '''
+    """
     train_losses, test_losses, train_accuracies, test_accuracies = [], [], [], []
     for epoch in tqdm.tqdm(range(n_epochs), total=n_epochs, leave=True):
         # Переводим сеть в режим обучения
@@ -20,25 +20,24 @@ def training_loop(n_epochs, network, loss_fn, optimizer, ds_train, ds_test, devi
 
         # Итерация обучения сети
         def closure():
-            '''
+            """
             Функция-замыкания для подсчёта градиентов функции потерь по обучающей выборке:
                 1. Очистка текущих градиентов
                 2. Выполнение прямого прохода по сети в вычисление функции потерь
                 3. Вычисление градиентов функции потерь
             :returns: Значение функции потерь
-            '''
+            """
             optimizer.zero_grad()
-            
+
             pred = network(ds_train[0].float().to(device))
             loss = loss_fn(pred, ds_train[1].to(device))
 
             loss.backward()
             return loss
 
-        
         # Шаг оптимизации
         optimizer.step(closure)
-        
+
         # Переводим сеть в инференс режим
         network.eval()
 
@@ -48,24 +47,31 @@ def training_loop(n_epochs, network, loss_fn, optimizer, ds_train, ds_test, devi
             # Вычисление качества и функции потерь на обучающей выборке
             y_pred = network(ds_train[0].float().to(device))
             loss = loss_fn(y_pred, ds_train[1].to(device))
-            acc = torch.sum(torch.argmax(y_pred, 1) == ds_train[1].to(device)) / \
-                  ds_train[1].to(device).shape[0]
+            acc = (
+                torch.sum(torch.argmax(y_pred, 1) == ds_train[1].to(device))
+                / ds_train[1].to(device).shape[0]
+            )
             train_losses.append(loss)
             train_accuracies.append(acc * 100)
-            
+
             # Вычисление качества и функции потерь на тестовой выборке
             y_pred = network(ds_test[0].float().to(device))
             loss = loss_fn(y_pred, ds_test[1].to(device))
-            acc = torch.sum(torch.argmax(y_pred, 1) == ds_test[1].to(device)) / \
-                  ds_test[1].to(device).shape[0]
+            acc = (
+                torch.sum(torch.argmax(y_pred, 1) == ds_test[1].to(device))
+                / ds_test[1].to(device).shape[0]
+            )
             test_losses.append(loss)
             test_accuracies.append(acc * 100)
-            
+
             if epoch % 20 == 0:
                 print(
-                    'Loss (Train/Test): {0:.3f}/{1:.3f}. Accuracy, % (Train/Test): {2:.2f}/{3:.2f}'.format(
-                        train_losses[-1], test_losses[-1], train_accuracies[-1], test_accuracies[-1]
+                    "Loss (Train/Test): {0:.3f}/{1:.3f}. Accuracy, % (Train/Test): {2:.2f}/{3:.2f}".format(
+                        train_losses[-1],
+                        test_losses[-1],
+                        train_accuracies[-1],
+                        test_accuracies[-1],
                     )
                 )
-            
+
     return train_losses, test_losses, train_accuracies, test_accuracies
